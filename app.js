0dcd1c56a4dd4455389dbf7c76111c7f3c18fd37
elraffa <federicoruffa@gmail.com>
2020-08-20 19:40:54 -0300

    Added port variable

--- app.js
+++ app.js
@@ -1,21 +1,24 @@
 var express = require('express');
-var exphbs  = require('express-handlebars');
- 
+var exphbs = require('express-handlebars');
+const PORT = process.env.PORT || 3000;
+
 var app = express();
- 
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('home');
+  res.render('home');
 });
 
 app.get('/detail', function (req, res) {
-    res.render('detail', req.query);
+  res.render('detail', req.query);
 });
 
 app.use(express.static('assets'));
- 
+
 app.use('/assets', express.static(__dirname + '/assets'));
- 
-app.listen(3000);
+
+app.listen(PORT, () => {
+  console.log(`Our app is running on port ${PORT}`);
+});
